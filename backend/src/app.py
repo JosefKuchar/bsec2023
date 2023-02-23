2a8ec139b795be405b628cfd190f4104ac402864
Lukas Vecerka <vecerkalukas18@gmail.com>
2023-02-23 22:58:11 +0100

    change methods for some endpoints

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,8 +1,6 @@
 from flask import Flask, jsonify, request
 from models import db
 from flask_cors import CORS
-import json
-
 
 # create the app
 app = Flask(__name__)
@@ -20,7 +18,7 @@ def home():
         data = "hello world"
         return jsonify({'data': data})
 
-@app.route('/restaurants', methods=['GET', 'POST'])
+@app.route('/restaurants', methods=['GET'])
 def restaurants():
     restaurations = Restaurant.query.all()
     restauration_list = []
@@ -31,7 +29,7 @@ def restaurants():
         })
     return jsonify(restauration_list)
 
-@app.route('/food', methods=['GET', 'POST'])
+@app.route('/food', methods=['GET'])
 def restaurants_food():
     if request.method == 'GET':
         param = request.args.get('restaurant_id')
@@ -47,7 +45,7 @@ def restaurants_food():
 
     return jsonify(food_list)
 
-@app.route('/records', methods=['GET', 'POST'])
+@app.route('/records', methods=['GET'])
 def records():
     if request.method == 'GET':
         records = RecordData.query.all()
